ff154608e23befefad6344d0a80899c3ab1a0537
SGJki <gjki5995@gmail.com>
2026-03-09 13:58:53 +0800

    init

---
+++ src/main/java/org/sgj/rljobscheduler/RlJobSchedulerApplication.java
@@ -0,0 +1,13 @@
+package org.sgj.rljobscheduler;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RlJobSchedulerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RlJobSchedulerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/sgj/rljobscheduler/RlJobSchedulerApplicationTests.java
@@ -0,0 +1,13 @@
+package org.sgj.rljobscheduler;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RlJobSchedulerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
